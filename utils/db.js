7408294a4962f8bca333f1b65f63c11ce482aebd
Caroll1889 <diahancaroll@hotmail.com>
2021-06-04 16:57:29 -0500

    Fix: eslint

--- utils/db.js
+++ utils/db.js
@@ -7,40 +7,37 @@ const url = `mongodb://${HOST}:${PORT}`;
 
 class DBClient {
   constructor() {
-    
     MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-        if (!err) {
+      if (!err) {
         this.db = client.db(DATABASE);
         this.userCol = this.db.collection('users');
         this.filesCol = this.db.collection('files');
       } else {
         console.log(err.message);
         this.db = false;
-        return;
       }
     });
-}
+  }
 
-    // Function that checks if connection to MongoDB is a success
-    isAlive() {
-        if (this.db) {
-            return true;
-        } else {
-            return false
-        }
+  // Function that checks if connection to MongoDB is a success
+  isAlive() {
+    if (this.db) {
+      return true;
     }
+    return false;
+  }
 
-    // Function that returns the number of documents in the
-    // collection users
-    async nbUsers() {
-        return this.userCol.countDocuments()
-    }
+  // Function that returns the number of documents in the
+  // collection users
+  async nbUsers() {
+    return this.userCol.countDocuments();
+  }
 
-    // Function that returns the number of documents in the
-    // collection files
-    async nbFiles() {
-        return this.filesCol.countDocuments()
-    }
+  // Function that returns the number of documents in the
+  // collection files
+  async nbFiles() {
+    return this.filesCol.countDocuments();
+  }
 }
 const dbClient = new DBClient();
 export default dbClient;
