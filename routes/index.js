25ddd759434b8778ea67c5c5c3b5656da322fb8f
arabellak <barbaracalle10i3@gmail.com>
2021-06-08 16:04:05 -0500

    Added the connect, disconnect and users/me endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,33 @@
-const express = require('express');
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 
-const router = express();
+const express = require('express');
 
-//Endpoints
-router.get('/status', function(req, res) {
-    AppController.getStatus(req, res);
+const router = express.Router();
+
+// Endpoints
+router.get('/status', (req, res) => {
+  AppController.getStatus(req, res);
+});
+
+router.get('/stats', (req, res) => {
+  AppController.getStats(req, res);
+});
+
+router.post('/users', (req, res) => {
+  UsersController.postNew(req, res);
+});
+
+router.get('/connect', function(req, res) {
+  AuthController.getConnect(req, res);
 })
 
-router.get('/stats', function (req, res) {
-    AppController.getStats(req, res);
+router.get('/disconnect', function(req, res) {
+  AuthController.getDisconnect(req, res);
 })
 
-router.post('/users', function(req, res) {
-    UsersController.postNew(req, res)
+router.get('/users/me', function(req, res) {
+  UserController.getMe(req, res);
 })
 
 module.exports = router;
