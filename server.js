9938c0363c7d04bbf442518f9f5ed8600017616a
arabellak <barbaracalle10i3@gmail.com>
2021-06-09 18:45:26 -0500

    Removed the routes

--- server.js
+++ server.js
@@ -2,22 +2,12 @@ import express from 'express';
 import routeControllers from './routes/index';
 
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
 // All routes from routes directory
 routeControllers(app);
-
-app.use(express.json());
-
-app.use('/', router);
-app.use('/status', router);
-app.use('/stats', router);
-app.use('/connect', router);
-app.use('/disconnect', router);
-app.use('/users/me', router);
-app.use(`/files/${id}`, route);
-app.use('/files ', route);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
