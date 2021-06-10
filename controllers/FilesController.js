23ed5e77506cb18a48908aa3f58d357725ac5342
Caroll1889 <diahancaroll@hotmail.com>
2021-06-09 20:01:10 -0500

    Fix: eslint

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,67 +1,64 @@
+import { ObjectId } from 'mongodb';
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
-import { ObjectId } from 'mongodb';
-import { v4 as uuid } from 'uuid';
-
 
 class FilesController {
-    static postUpload(req, res) {
-      const token = req.header('X-Token');
-      if (!token) return res.status(401).send({ error: 'Unauthorized' })
-      
-      //Obtain and verify an user in Redis
-      const userId = redisClient.get(`auth_${token}`)
-      if (!userId) return res.status(401).send({ error: 'Unauthorized' })
+  static postUpload(req, res) {
+    const token = req.header('X-Token');
+    if (!token) return res.status(401).send({ error: 'Unauthorized' });
 
-      //Obtain and verify an user in MongoDb
-      const user = dbClient.db.collection('users').findOne({ id: ObjectId(userId) });
-      if (!user) return res.status(401).send({ error: 'Unauthorized' })
+    // Obtain and verify an user in Redis
+    const userId = redisClient.get(`auth_${token}`);
+    if (!userId) return res.status(401).send({ error: 'Unauthorized' });
 
-      //Create a file
+    // Obtain and verify an user in MongoDb
+    const user = dbClient.db.collection('users').findOne({ id: ObjectId(userId) });
+    if (!user) return res.status(401).send({ error: 'Unauthorized' });
 
-      //Name
-      const fileName = req.body.name
-      if (!fileName) return res.status(400).send({ error: 'Missing name' })
+    // Create a file
 
-      //Type
-      const fileType = req.body.type
-      if (!fileType || !['folder', 'file', 'image'].includes(fileType)) return res.status(400).send({ error: 'Missing type' })
+    // Name
+    const fileName = req.body.name;
+    if (!fileName) return res.status(400).send({ error: 'Missing name' });
 
-      //Data
-      const fileData = req.body.data
-      if (!fileData && ['file', 'image'].includes(fileType)) return res.status(400).send({ error: 'Missing data' })
+    // Type
+    const fileType = req.body.type;
+    if (!fileType || !['folder', 'file', 'image'].includes(fileType)) return res.status(400).send({ error: 'Missing type' });
 
-      const filePublic = req.body.isPublic || false
-      const fileParentId = req.body.parentId || 0
-      
-      if (fileParentId !== 0) {
-        const parentFile = dbClient.db.collection('files').findOne({ _id: ObjectId(fileParentId) });
-        if (!parentFile) return res.status(400).send({ error: 'Parent not found' });
-        if (!['folder'].includes(parentFile.type)) return res.status(400).send({ error: 'Parent is not a folder' });
-      }
+    // Data
+    const fileData = req.body.data;
+    if (!fileData && ['file', 'image'].includes(fileType)) return res.status(400).send({ error: 'Missing data' });
 
-      const fileDb = {
-        userId: user._id,
-        name: fileName,
-        type: fileType,
-        isPublic: filePublic,
-        parentId: fileParentId,
-      }
-      if (['folder'].includes(fileType)) {
-        dbClient.db.collection('files').insertOne(fileDb)
-        return res.status(201).send({
-          id: fileDb._id,
-          userId: fileDb.userId,
-          name: fileDb.name,
-          type: fileDb.type,
-          isPublic: fileDb.isPublic,
-          parentId: fileDb.fileParentId
-        })
-      }
+    const filePublic = req.body.isPublic || false;
+    const fileParentId = req.body.parentId || 0;
 
-      const allFiles = process.env.FOLDER_PATH || '/tmp/files_manager';
-      
+    if (fileParentId !== 0) {
+      const parentFile = dbClient.db.collection('files').findOne({ _id: ObjectId(fileParentId) });
+      if (!parentFile) return res.status(400).send({ error: 'Parent not found' });
+      if (!['folder'].includes(parentFile.type)) return res.status(400).send({ error: 'Parent is not a folder' });
     }
+
+    const fileDb = {
+      userId: user._id,
+      name: fileName,
+      type: fileType,
+      isPublic: filePublic,
+      parentId: fileParentId,
+    };
+    if (['folder'].includes(fileType)) {
+      dbClient.db.collection('files').insertOne(fileDb);
+      return res.status(201).send({
+        id: fileDb._id,
+        userId: fileDb.userId,
+        name: fileDb.name,
+        type: fileDb.type,
+        isPublic: fileDb.isPublic,
+        parentId: fileDb.fileParentId,
+      });
+    }
+
+    // const allFiles = process.env.FOLDER_PATH || '/tmp/files_manager';
+  }
 }
 
 export default FilesController;
