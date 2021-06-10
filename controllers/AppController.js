49f76d89e4516d8e06d62f1d9853a28fc0fe49cc
arabellak <barbaracalle10i3@gmail.com>
2021-06-09 21:23:59 -0500

    Removed the try and catch

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -6,25 +6,17 @@ class AppController {
   // Return if Redis and DB are alive by using
   // the two utils done previously with status code 200
   static getStatus(req, res) {
-    try {
-      const redis = redisClient.isAlive();
-      const database = dbClient.isAlive();
-      res.status(200).send({ redis, database });
-    } catch (err) {
-      console.log(err);
-    }
+    const redis = redisClient.isAlive();
+    const database = dbClient.isAlive();
+    res.status(200).send({ redis, database });
   }
 
   // Return the number of users and files in DB
   // with status code 200
   static async getStats(req, res) {
-    try {
-      const user = await dbClient.nbUsers();
-      const file = await dbClient.nbFiles();
-      res.status(200).send({ user, file });
-    } catch (err) {
-      console.log(err);
-    }
+    const user = await dbClient.nbUsers();
+    const file = await dbClient.nbFiles();
+    res.status(200).send({ user, file });
   }
 }
 
